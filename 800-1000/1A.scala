4625bf41145cdcd77d4d574f57076594d47e1c5c
eduarda <eduarda.lacerda@nubank.com.br>
2023-05-19 14:08:19 -0300

    solving 1A problem

---
+++ 800-1000/1A.scala
@@ -0,0 +1,17 @@
+/** This problem is rated as 800 in Codeforces.
+ *  https://codeforces.com/problemset/problem/1/A
+ */
+
+import scala.io.StdIn._
+
+object Solve extends App {
+    val w = readLine
+
+    val (n, m, a): (Long, Long, Long) = w.split(" ").map(_.toLong).toSeq match {
+        case Seq(a, b, c, _*) => (a, b, c)
+    }
+    
+    val (d, e) = ((n.toDouble/a).ceil, (m.toDouble/a).ceil)
+    
+    println((d*e).toLong)
+}
